bab6984a1393f884e761fe011ade80cc9b5e8f91
NikiSub <2018nsubrama@local.tjhsst.edu>
2017-06-05 08:43:08 -0400

    Hi

--- app/src/main/java/com/kaiamelung/debrief/ChooseTagActivity.java
+++ app/src/main/java/com/kaiamelung/debrief/ChooseTagActivity.java
@@ -3,14 +3,21 @@ package com.kaiamelung.debrief;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class ChooseTagActivity extends AppCompatActivity {
-    //private String tagArrrayAvailable[] = new String[R.integer.tagNums];
-    //private String tagArrrayChosen[] = new String[R.integer.tagNums];
+    private ArrayList<String> tagArrayAvailable;
+    private ArrayList<String> tagArrayChosen;
+    private String[] tags;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_tag);
+        tagArrayAvailable = new ArrayList<String>(getResources().getInteger(R.integer.tagNums));
+        tagArrayChosen = new ArrayList<String>(getResources().getInteger(R.integer.tagNums));
+        tags = getResources().getStringArray(R.array.tags);
+
 
     }
 }
