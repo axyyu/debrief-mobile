9fd90233a61df7f4e2a6020c0db5864c1b12973b
DESKTOP\Andrew Wang <andrewxiaoyu0@gmail.com>
2017-08-09 14:54:58 -0400

    Created new main and settings pages, added colors

---
+++ app/src/main/java/com/kaiamelung/debrief/Setting.java
@@ -0,0 +1,30 @@
+package com.kaiamelung.debrief;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Setting extends AppCompatActivity {
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        SHOW BACK BUTTON
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+}
